80783cdb4a1ee196d75508b325ed84f50faa4c2f
encryptedDegen <jan.zunec@sers.si>
2023-06-18 22:07:25 +0200

    few additions

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./NavBar.css";
 import { MdOutlineCoronavirus, MdInfoOutline } from "react-icons/md";
+import { RiGithubLine } from "react-icons/ri";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
     <div className="navigation">
       <div className="logo">
-        <MdOutlineCoronavirus />
+        <Link to="/">
+          <MdOutlineCoronavirus />
+        </Link>
       </div>
       <ul className="links-list">
         <li className="link">
           <Link to="/info">
             <MdInfoOutline />
           </Link>
+        </li>
+        <li className="link">
+          <a target="blank" href="https://github.com/Janzunec/covstats19">
+            <RiGithubLine />
+          </a>
         </li>
       </ul>
     </div>
--- src/pages/Info/Info.js
+++ src/pages/Info/Info.js
@@ -5,6 +5,18 @@ export default function Info() {
   return (
     <div className="info">
       <h1>Covstats-19</h1>
+      <p>
+        Pojav virusa Covid-19 je leta 2020 povzročilo globalno krizo, saj se je
+        virus hitro razširil in ušel izpod nadzora. Ljudje v času krize želijo
+        izvedeti kolikor informacij je mogoče. Zaradi tega razloga so se
+        pojavile obstoječe spletne strani, kjer so prikazane statistike, ki so
+        pomembne ljudem, vendar pa so te bile hitro narejene in po mojem mnenju
+        se ne osredotočajo na prijazno uporabniško izkušnjo. Primanjkuje jim
+        načinov prikazovanje informacij, kar je tudi mene močno zmotilo. Odločil
+        sem se, da bom sam izdelal spletno stran, ki omogoča pregled statistik
+        virusa Covid-19 celega sveta (aktivni primer, ozdravljeni in smrtne
+        žrtve) v modernem in uporabnem izgledu, ki je prijazen uporabniku
+      </p>
     </div>
   );
 }
